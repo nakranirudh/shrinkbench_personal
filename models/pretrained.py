94327ba90801b21b5a5ebc33656d8d3772e5f2a1
Ani <84345588+nakranirudh@users.noreply.github.com>
2023-11-21 11:10:03 -0500

    Adding pretrained MNIST

--- models/pretrained.py
+++ models/pretrained.py
@@ -4,3 +4,8 @@ def pretrained_weights(model):
     url = f'https://raw.githubusercontent.com/JJGO/shrinkbench-models/master/cifar10/{model}.th'
     print(url)
     return torch.hub.load_state_dict_from_url(url, map_location='cpu')
+
+def pretrained_weights_mnist(model):
+    url = f'https://raw.githubusercontent.com/JJGO/shrinkbench-models/master/cifar10/{model}.th'
+    print(url)
+    return torch.hub.load_state_dict_from_url(url, map_location='cpu')
